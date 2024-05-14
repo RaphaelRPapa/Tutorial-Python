ec3d3c267cc1757e244229f6ecd239ea0b636119
Raphael Rodrigues Papa <86625359+RaphaelRPapa@users.noreply.github.com>
2024-05-13 21:04:29 -0300

    Add Etapa 1 files

---
+++ Tutorial/Etapa 1- Conceitos básicos/variaveis.py
@@ -0,0 +1,13 @@
+# Exemplo de variáveis e tipos de dados em Python
+
+# Variáveis e tipos de dados
+nome = "Maria"     # string
+idade = 25         # int
+altura = 1.65      # float
+tem_filhos = False # bool
+
+# Imprimindo valores das variáveis
+print("Nome:", nome)
+print("Idade:", idade)
+print("Altura:", altura)
+print("Tem filhos?", tem_filhos)
